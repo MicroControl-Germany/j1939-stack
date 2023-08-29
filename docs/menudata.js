297795ea2f0e2cbac0876fd102a70242a3c79a5c
Uwe Koppe <koppe@microcontrol.net>
2023-08-29 19:32:22 +0200

    Add style

---
+++ docs/menudata.js
@@ -0,0 +1,66 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var menudata={children:[
+{text:"Main Page",url:"index.html"},
+{text:"Getting Started",url:"",children:[
+{text:"First steps",url:"j1939_getting_started.html"},
+{text:"Protocol Stack Overview",url:"j1939_api_overview.html"},
+{text:"Code Examples",url:"j1939_examples.html",children:[
+{text:"Initialisation",url:"j1939_example_start.html"},
+{text:"PGN handling",url:"j1939_example_pgn.html"},
+{text:"ECU demo",url:"j1939_example_ecu1.html"},
+{text:"Joystick demo",url:"j1939_example_joystick.html"}]},
+{text:"Template Files",url:"j1939_api_templates.html",children:[
+{text:"J1939 configuration",url:"j1939__conf_8h.html#J1939_CONFIG"},
+{text:"J1939 start",url:"j1939_example_main.html"},
+{text:"J1939 callbacks",url:"j1939_example_user.html"}]}]},
+{text:"Reference",url:"",children:[
+{text:"J1939 Stack",url:"j1939_api_stack.html",children:[
+{text:"BAM service",url:"j1939__bam_8h.html#J1939_BAM"},
+{text:"CANopen tunnel",url:"j1939__cos__tunnel_8h.html#J1939_COS_TUNNEL"},
+{text:"Definitions",url:"j1939__defs_8h.html#J1939_DEFS"},
+{text:"Diagnostics",url:"j1939__diag_8h.html#J1939_DIAG"},
+{text:"Management",url:"j1939__mgr_8h.html#J1939_MGR"},
+{text:"PDU support",url:"j1939__msg_8h.html#J1939_MSG"},
+{text:"NMT service",url:"j1939__nmt_8h.html#J1939_NMT"},
+{text:"PGN support",url:"j1939__pgn_8h.html#J1939_PGN"},
+{text:"Timer support",url:"j1939__tmr_8h.html#J1939_TMR"},
+{text:"TP service",url:"j1939__tp_8h.html#J1939_TP"}]},
+{text:"CANpie FD",url:"j1939_api_canpie.html",children:[
+{text:"Core",url:"cp__core_8h.html#CP_CORE_H"},
+{text:"FIFO",url:"cp__fifo_8h.html#CP_FIFO_H"},
+{text:"Message",url:"cp__msg_8h.html#CP_MSG_H"},
+{text:"Structures",url:"canpie_8h.html#CANPIE_H"}]},
+{text:"Hardware Abstraction Layer",url:"j1939_api_hal.html",children:[
+{text:"CPU",url:"mc__cpu_8h.html#MC_CPU"},
+{text:"Timer",url:"mc__tmr_8h.html#MC_TMR"},
+{text:"Watchdog",url:"mc__wdt_8h.html#MC_WDT"},
+{text:"Compiler definitions",url:"mc__compiler_8h.html#MC_COMPILER_H"}]},
+{text:"Version history",url:"j1939_version.html"}]},
+{text:"Overviews",url:"",children:[
+{text:"Introduction to CAN",url:"intro_can.html"},
+{text:"References",url:"stack_references.html"},
+{text:"Support",url:"stack_support.html"},
+{text:"License",url:"page_8license.html"}]}]}
---
+++ docs/navtreedata.js
@@ -0,0 +1,85 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "J1939 Protocol Stack", "index.html", [
+    [ "Getting Started", null, [
+      [ "First steps", "j1939_getting_started.html", null ],
+      [ "Protocol Stack Overview", "j1939_api_overview.html", null ],
+      [ "Code Examples", "j1939_examples.html", [
+        [ "Initialisation", "j1939_example_start.html", null ],
+        [ "PGN handling", "j1939_example_pgn.html", null ],
+        [ "ECU demo", "j1939_example_ecu1.html", null ],
+        [ "Joystick demo", "j1939_example_joystick.html", null ]
+      ] ],
+      [ "Template Files", "j1939_api_templates.html", [
+        [ "J1939 configuration", "j1939__conf_8h.html#J1939_CONFIG", null ],
+        [ "J1939 start", "j1939_example_main.html", null ],
+        [ "J1939 callbacks", "j1939_example_user.html", null ]
+      ] ]
+    ] ],
+    [ "Reference", null, [
+      [ "J1939 Stack", "j1939_api_stack.html", [
+        [ "BAM service", "j1939__bam_8h.html#J1939_BAM", null ],
+        [ "CANopen tunnel", "j1939__cos__tunnel_8h.html#J1939_COS_TUNNEL", null ],
+        [ "Definitions", "j1939__defs_8h.html#J1939_DEFS", null ],
+        [ "Diagnostics", "j1939__diag_8h.html#J1939_DIAG", null ],
+        [ "Management", "j1939__mgr_8h.html#J1939_MGR", null ],
+        [ "PDU support", "j1939__msg_8h.html#J1939_MSG", null ],
+        [ "NMT service", "j1939__nmt_8h.html#J1939_NMT", null ],
+        [ "PGN support", "j1939__pgn_8h.html#J1939_PGN", null ],
+        [ "Timer support", "j1939__tmr_8h.html#J1939_TMR", null ],
+        [ "TP service", "j1939__tp_8h.html#J1939_TP", null ]
+      ] ],
+      [ "CANpie FD", "j1939_api_canpie.html", [
+        [ "Core", "cp__core_8h.html#CP_CORE_H", null ],
+        [ "FIFO", "cp__fifo_8h.html#CP_FIFO_H", null ],
+        [ "Message", "cp__msg_8h.html#CP_MSG_H", null ],
+        [ "Structures", "canpie_8h.html#CANPIE_H", null ]
+      ] ],
+      [ "Hardware Abstraction Layer", "j1939_api_hal.html", [
+        [ "CPU", "mc__cpu_8h.html#MC_CPU", null ],
+        [ "Timer", "mc__tmr_8h.html#MC_TMR", null ],
+        [ "Watchdog", "mc__wdt_8h.html#MC_WDT", null ],
+        [ "Compiler definitions", "mc__compiler_8h.html#MC_COMPILER_H", null ]
+      ] ],
+      [ "Version history", "j1939_version.html", null ]
+    ] ],
+    [ "Overviews", null, [
+      [ "Introduction to CAN", "intro_can.html", null ],
+      [ "References", "stack_references.html", null ],
+      [ "Support", "stack_support.html", null ],
+      [ "License", "page_8license.html", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"canpie_8h.html#CANPIE_H"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
